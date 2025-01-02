0227c5fc38fb42a8f70dc81c1d2af2a1151e5e54
BodaLuciaAndres <luciayandresboda@gmail.com>
2025-01-02 17:36:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
 const express = require("express");
-const axios = require("axios");
+const cloudinary = require("cloudinary").v2;
 const cors = require("cors");
 
 const app = express();
 app.use(cors()); // Habilitar CORS
 
+// Configurar Cloudinary
+cloudinary.config({
+  cloud_name: "drbvcuaak",
+  api_key: "285915571483646",
+  api_secret: "xhFxxPOHrx9mqNNW3p1K2q3_w0k",
+});
+
 app.get("/getImages", async (req, res) => {
   try {
-    const response = await axios.get(
-      "https://api.cloudinary.com/v1_1/drbvcuaak/resources/image",
-      {
-        params: {
-          folder: "guest",
-          max_results: 10,
-        },
-        headers: {
-          "Authorization": `Basic ${Buffer.from(
-            "285915571483646:xhFxxPOHrx9mqNNW3p1K2q3_w0k",
-          ).toString("base64")}`,
-        },
-      }
-    );
-
-    res.json(response.data);
+    const resources = await cloudinary.api.resources({
+      type: "upload",
+      prefix: "guest", // Carpeta en Cloudinary
+      max_results: 10,
+    });
+    res.json(resources);
   } catch (error) {
     console.error("Error al obtener imágenes:", error);
     res.status(500).send("Error al obtener imágenes");
